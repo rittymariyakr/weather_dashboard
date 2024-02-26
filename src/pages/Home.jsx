271630df5b51cfe86de111ff7ba0d142ea7f9932
rittymariyakr <rittymariyakr@gmail.com>
2024-02-26 10:58:06 +0530

    waether dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ function Home() {
             <div style={{ backgroundImage: "url(https://media.istockphoto.com/id/1368170023/photo/hurricane-ida-2021-topographic-map-3d-render-color.webp?b=1&s=170667a&w=0&k=20&c=kakXmBYTp2KF4WUliCYNIsvwBwGdTbU2aVEkZ3ONop0=)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '680px' }}>
                 <div style={{ height: '600px' }} className='d-flex  align-items-center '>
                     <Container >
-                        <h1 style={{ fontWeight: 'bold', fontSize: '50px', color: 'black' }}>The cars we drive says <br></br>a lot about us </h1>
-                        <Link style={{ textDecoration: 'none' }} to={'/login'}><button style={{ backgroundColor: 'black', color: 'white' }} className='btn rounded'><b>Show Cars</b></button></Link>
+                        <h1 style={{ fontWeight: 'bold', fontSize: '50px', color: 'black' }}>The Weather Says <br></br>a lot </h1>
+                        <Link style={{ textDecoration: 'none' }} to={'/login'}><button style={{ backgroundColor: 'black', color: 'white' }} className='btn rounded'><b>Get Started</b></button></Link>
 
                     </Container>
 
